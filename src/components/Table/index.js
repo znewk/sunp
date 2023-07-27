bea7e4b8b2c2132d4e85ff40e0a0d6203a1a174b
Arlan <zane.css34@gmail.com>
2023-07-27 16:08:51 +0600

    tables adaptation

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -6,11 +6,13 @@ const Table = ({title, info, ...props}) => {
         <div className={styles.container}>
             <h2 className={styles.title}>{title}</h2>
 
-            <table className={styles.table}>
-                {
-                    title == "Цены на нефтепродукты" && <NeftPricesTable info={info}/>
-                }
-            </table>
+            <div className={styles.tableBody}>
+                <table className={styles.table}>
+                    {
+                        title == "Цены на нефтепродукты" && <NeftPricesTable info={info}/>
+                    }
+                </table>
+            </div>
         </div>
     )
 }
